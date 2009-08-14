b2249c15d4fe63d9c86511adc585dbb528a55de7
craig.cook@bt.com <craig.cook@bt.com@ecdc356e-9e51-0410-bef0-db38c23c9c74>
2009-08-14 14:04:18 +0000

    CCDSO-591 testing helper class
    git-svn-id: https://collaborate.bt.com/svn/bt-dso/giovanni/trunk@10149 ecdc356e-9e51-0410-bef0-db38c23c9c74

--- lib/utils/helper.rb
+++ lib/utils/helper.rb
@@ -1,5 +1,5 @@
 class Helper
   def url_name
-    fetch(:war_file_name, fetch(:application))
+    fetch(:url_name, fetch(:application))
   end  
 end
--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -3,10 +3,17 @@ require File.join(File.dirname(__FILE__), "../lib/utils/helper")
 describe Helper do
   before(:each) do
     @helper = Helper.new
-    @helper.stub!(:fetch).and_return("foo")    
   end
 
-  it "should get url" do
-    @helper.url_name.should == 'foo'
+  it "should return 'application' if 'url_name' is not defined" do
+    @helper.stub!(:fetch).with(:application).and_return('application')
+    @helper.stub!(:fetch).with(:url_name, 'application').and_return('application')
+    @helper.url_name.should == 'application'
+  end
+
+  it "should return 'url_name' if 'url_name' is defined" do
+    @helper.stub!(:fetch).with(:application).and_return('application')
+    @helper.stub!(:fetch).with(:url_name, 'application').and_return('url_name')
+    @helper.url_name.should == 'url_name'
   end
 end
